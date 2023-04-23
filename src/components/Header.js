a6234bd08398fd247a84b73dfd0f726c7c34023c
Denis Efimov <defimov9@gmail.com>
2023-04-23 20:44:11 +0500

    feat/ add Routes to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,38 @@
-function Header() {
+import { Link, Route, Routes } from 'react-router-dom';
+
+function Header({ email }) {
   return (
     <header className='header'>
       <div className='header__logo' />
+      <Routes>
+        <Route
+          path='/sign-in'
+          element={
+            <Link className='header__link' to='/sign-in'>
+              Войти
+            </Link>
+          }
+        />
+        <Route
+          path='/sign-up'
+          element={
+            <Link className='header__link' to='/sign-up'>
+              Регистрация
+            </Link>
+          }
+        />
+        <Route
+          path='/'
+          element={
+            <div className='header__info-wrapper'>
+              <p className='header__link_email'>{email}</p>
+              <button className='header__exit' type='button'>
+                Выйти
+              </button>
+            </div>
+          }
+        />
+      </Routes>
     </header>
   );
 }
